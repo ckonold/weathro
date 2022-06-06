540f0cf1ed4bf99fb0c592095ae28dd00ef52324
Christian Konold <christiankonold@gmail.com>
2022-06-06 11:24:16 -0700

    added weather retrieval and storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { useState, useEffect } from "react";
 function App() {
-  const locStor = useState();
-  const units = useState("metric");
-  
-  const getLocation = async() => {
+  const [weather, setWeather] = useState();
+  const [units, setUnits] = useState("metric");
+
+  const getLocation = async () => {
     const pos = await new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject);
     });
@@ -16,29 +16,34 @@ function App() {
     return loc;
   };
 
-
-
-  const getWeather = async (lat,lon) => {
+  const getWeather = async (lat, lon) => {
     const fiveDay = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=eb665be38d94d86f6e3a39b4b657ed09`
-    )
-      let response = await fiveDay.json()
-      return response;
+    );
+    let response = await fiveDay.json();
+    return response;
+  };
+
+  const checkStorage = async () => {
+    if (localStorage.getItem("weather") == null) {
+      let loc = await getLocation();
+      let w = await getWeather(loc.lat, loc.lon);
+      setWeather(w);
+      localStorage.setItem("weather", weather);
+      console.log("Storage set!");
+    }
+  };
+  useEffect(() => {
+    checkStorage();
+  }, []);
+
+  function r() {
+    console.log(weather);
   }
-
-  const setStorage = () => {
-
-  }
-
-  const checkStorage = () => {
-
-  }
-
-
 
   return (
     <div className="App">
-     
+      <button onClick={r}>Console log</button>
     </div>
   );
 }
